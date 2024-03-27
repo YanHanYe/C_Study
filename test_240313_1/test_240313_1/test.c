1ebf69bebfc844609f215262532442b1611de07a
YanHanYe <taozhenyu0312@163.com>
2024-03-27 18:57:53 +0800

    导入C语言学习文件

---
+++ test_240313_1/test_240313_1/test.c
@@ -0,0 +1,89 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	int input = 0;
+//	printf("加入比特\n");
+//	printf("要好好学习吗？(1/0)\n");
+//	scanf("%d", &input);
+//	if (input == 1)
+//	{
+//		printf("好工作\n");
+//	}
+//	else
+//	{
+//		printf("回家卖红薯\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int line = 0;
+//	printf("加入代码\n");
+//	while (line < 20000)
+//	{
+//		printf("写代码:%d\n", line);
+//		line++;
+//	}
+//	if (line >= 20000)
+//	{
+//		printf("得到好offer\n");
+//	}
+//	else
+//	{
+//		printf("继续加油\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n1 = 0;
+//	int n2 = 0;
+//	//输入
+//	scanf("%d %d", &n1, &n2);
+//	//求和
+//	int sum = n1 + n2;
+//	//打印
+//	printf("%d", sum);
+//	return 0;
+//}
+
+
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int n1 = 0;
+//	int n2 = 0;
+//	scanf("%d %d", &n1, &n2);
+//	int sum = Add(n1, n2);
+//	printf("%d", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+//	//int i = 0;
+//	//while (i < 10)
+//	//{
+//	//	printf("%d\n", arr[i]);
+//	//	i = i + 1;
+//	//}
+//	printf("abc\\0defg");
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
